eb7471f1076919526b96cf04b066157e705ea33c
Youssef <youssef550@live.com>
2017-03-22 20:06:09 -0400

    Ajustement JPanel

--- L4_CODE_YAbdellaoui_RJobin_YSoliman_VTrinh/src/vue/Fenetre.java
+++ L4_CODE_YAbdellaoui_RJobin_YSoliman_VTrinh/src/vue/Fenetre.java
@@ -2,7 +2,6 @@ package vue;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,18 +11,16 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 
 import java.awt.SystemColor;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.GridLayout;
 
 public class Fenetre extends JFrame {
-	private JPanel jp1;
-	private JPanel jp2;
-	private JPanel jp3;
-	private JPanel jp4;
-	private JPanel jp5;
-	private JPanel jp6;
+	
 	private JMenuBar menuBar;
 	private JMenu mnFichier;
 	private JMenuItem mntmOuvrir;
@@ -34,11 +31,18 @@ public class Fenetre extends JFrame {
 	private JMenu mnEditer;
 	private JMenuItem mntmUndo;
 	private JMenuItem mntmRedo;
+	private JPanel jp;
+	private JPanel jp1;
+	private JPanel jp2;
+	private JPanel jpPerspective1;
+	private JPanel jpPerspective2;
+	private JPanel jpThumbnail;
 	
 	public Fenetre() {
 		super("Application Image");
 		
 		getContentPane().setBackground(SystemColor.activeCaption);
+		getContentPane().setLayout(new GridLayout(1, 1));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setPreferredSize(new Dimension(1024,720));
 		
@@ -60,7 +64,6 @@ public class Fenetre extends JFrame {
 		pack();
 		
 		setVisible(true);
-		
 	}
 	
 	private void addMenuFichier() {
@@ -99,31 +102,76 @@ public class Fenetre extends JFrame {
 		mnEditer.add(mntmRedo);
 	}
 	private void addJPanels() {
+		jp = new JPanel();
 		jp1 = new JPanel();
 		jp2 = new JPanel();
-		jp3 = new JPanel();
-		jp4 = new JPanel();
-		jp5 = new JPanel();
-		jp6 = new JPanel();
+		jpThumbnail = new JPanel();
+		jpPerspective1 = new JPanel();
+		jpPerspective2 = new JPanel();
 		
-		jp1.setLayout(new GridLayout(1,2));
-		jp2.setLayout(new GridLayout(2,1));
-		jp3.setLayout(new GridLayout(2,1));
-		jp4.setLayout(new GridLayout(1, 1));
-		jp5.setLayout(new GridLayout(1, 1));
-		jp6.setLayout(new GridLayout(1, 1));
+		jp.setOpaque(false);
+		jp1.setOpaque(false);
+		jp2.setOpaque(false);
+		jpPerspective1.setBackground(new Color(0, 0, 0));
+		jpPerspective2.setBackground(new Color(0, 0, 0));
+		getContentPane().add(jp);
 		
 		
-		jp4.setBackground(Color.BLACK);
-		jp5.setBackground(Color.BLUE);
-		jp6.setBackground(Color.CYAN);
 		
-		getContentPane().add(jp1);
-		jp1.add(jp2);
-		jp1.add(jp3);
-		jp2.add(jp4);
-		jp2.add(jp5);
-		jp3.add(jp6);
+		
+		GroupLayout glJp = new GroupLayout(jp);
+		glJp.setHorizontalGroup(
+			glJp.createParallelGroup(Alignment.LEADING)
+				.addGroup(glJp.createSequentialGroup()
+					.addComponent(jp1, GroupLayout.PREFERRED_SIZE, 768, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(jp2, GroupLayout.DEFAULT_SIZE, 256, Short.MAX_VALUE))
+		);
+		glJp.setVerticalGroup(
+			glJp.createParallelGroup(Alignment.LEADING)
+				.addComponent(jp1, GroupLayout.DEFAULT_SIZE, 660, Short.MAX_VALUE)
+				.addComponent(jp2, GroupLayout.DEFAULT_SIZE, 660, Short.MAX_VALUE)
+		);
+		
+		
+		jpThumbnail.setBackground(new Color(0, 0, 0));
+		GroupLayout glThumbnail = new GroupLayout(jp2);
+		glThumbnail.setHorizontalGroup(
+			glThumbnail.createParallelGroup(Alignment.LEADING)
+				.addGroup(glThumbnail.createSequentialGroup()
+					.addComponent(jpThumbnail, GroupLayout.DEFAULT_SIZE, 256, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		glThumbnail.setVerticalGroup(
+			glThumbnail.createParallelGroup(Alignment.LEADING)
+				.addGroup(glThumbnail.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(jpThumbnail, GroupLayout.PREFERRED_SIZE, 320, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(330, Short.MAX_VALUE))
+		);
+		jp2.setLayout(glThumbnail);
+		
+		
+		
+		GroupLayout glPerspectives = new GroupLayout(jp1);
+		glPerspectives.setHorizontalGroup(
+			glPerspectives.createParallelGroup(Alignment.LEADING)
+				.addGroup(glPerspectives.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(glPerspectives.createParallelGroup(Alignment.LEADING)
+						.addComponent(jpPerspective2, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 758, Short.MAX_VALUE)
+						.addComponent(jpPerspective1, GroupLayout.DEFAULT_SIZE, 758, Short.MAX_VALUE)))
+		);
+		glPerspectives.setVerticalGroup(
+			glPerspectives.createParallelGroup(Alignment.LEADING)
+				.addGroup(glPerspectives.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(jpPerspective1, GroupLayout.PREFERRED_SIZE, 320, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(jpPerspective2, GroupLayout.DEFAULT_SIZE, 320, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		jp1.setLayout(glPerspectives);
+		jp.setLayout(glJp);
 	}
-
 }
