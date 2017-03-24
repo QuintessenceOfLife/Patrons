fbd758abb07b32c6f8d95554589f7e1ba160ec72
Unknown <yassine.abdellaoui.1@etsmtl.net>
2017-03-24 15:43:49 -0400

    Code Zoom's constructor and add gestionnaire field to Commande class.

--- src/controller/Commande.java
+++ src/controller/Commande.java
@@ -1,6 +1,9 @@
 package controller;
 
 public abstract class Commande {
+
+	protected final static GestionnaireCmd gestionnaire = GestionnaireCmd.getGestionnaireCmd();
+    
 	public abstract boolean faire();
 	public abstract void defaire();
 }
