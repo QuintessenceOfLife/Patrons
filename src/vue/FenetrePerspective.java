afedb61ba30a78cf1e2315485d71024c1fb4df0b
Unknown <tda.victor@gmail.com>
2017-03-29 11:53:55 -0400

    Zoom test (in FenetrePerspective)
    Very laggy.

--- src/vue/FenetrePerspective.java
+++ src/vue/FenetrePerspective.java
@@ -1,6 +1,8 @@
 package vue;
 
 import java.awt.Image;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -17,11 +19,39 @@ public class FenetrePerspective extends JPanel implements Observateur {
 	
 	// Attributs
 	private Perspective perspectiveM; //le modèle de cette vue
+	JLabel picLabel;
+	private double zoom = 1.0;
 	
 	public FenetrePerspective(Perspective perspectiveM) {
 		this.perspectiveM = perspectiveM;  
 	   	this.perspectiveM.setObservateur(this); //Enregistre cette vue auprès de son modèle en tant qu'observateur	
+	    addMouseWheelListener(new MouseWheelListener() {
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                int notches = e.getWheelRotation();
+                double temp = zoom - (notches * 0.2);
+                // minimum zoom factor is 1.0
+                temp = Math.max(temp, 1.0);
+                if (temp != zoom) {
+                    zoom = temp;
+                    resizeImage();
+                }
+            }
+
+        });
 	}
+	
+	public void resizeImage() {
+        System.out.println(zoom);
+        BufferedImage myPicture;
+		try {
+			myPicture = ImageIO.read(Photo.getInstance().getFichierImage());
+			ImageIcon image = new ImageIcon(myPicture.getScaledInstance((int) (myPicture.getWidth() * zoom), (int) (myPicture.getHeight() * zoom), Image.SCALE_SMOOTH));
+	        picLabel.setIcon(image);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+     }
 
 	@Override
 	public void update() {
@@ -43,7 +73,7 @@ public class FenetrePerspective extends JPanel implements Observateur {
 				height = getSize().height;
 			}
 			
-			JLabel picLabel = new JLabel(new ImageIcon(myPicture.getScaledInstance(width, height, Image.SCALE_SMOOTH)));
+			picLabel = new JLabel(new ImageIcon(myPicture.getScaledInstance(width, height, Image.SCALE_SMOOTH)));
 			add(picLabel);
 			
 			revalidate();
@@ -54,4 +84,6 @@ public class FenetrePerspective extends JPanel implements Observateur {
 			e.printStackTrace();
 		}
 	}
+	
+	
 }
