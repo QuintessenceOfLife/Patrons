a96e8d9b0f8477ba7924a080e9619d6c68cfe936
Unknown <tda.victor@gmail.com>
2017-03-30 11:53:22 -0400

    Fix before merge

--- src/controller/Ouvrir.java
+++ src/controller/Ouvrir.java
@@ -1,11 +1,8 @@
 package controller;
 
 import java.io.File;
-import javax.imageio.ImageIO;
+
 import javax.swing.ImageIcon;
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileFilter;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Ouvrir extends Commande {
 	
@@ -20,11 +17,10 @@ public class Ouvrir extends Commande {
 	public boolean faire() {
 		photo.setFichierPhoto(file);
 			
-			ImageIcon image = new ImageIcon(file.getAbsolutePath());
-			
-			perspective1.setCoordinates(0, 0, image.getIconWidth(), image.getIconHeight());
-			perspective2.setCoordinates(0, 0, image.getIconWidth(), image.getIconHeight());
-		}
+		ImageIcon image = new ImageIcon(file.getAbsolutePath());
+		
+		perspective1.setCoordinates(0, 0, image.getIconWidth(), image.getIconHeight());
+		perspective2.setCoordinates(0, 0, image.getIconWidth(), image.getIconHeight());
 		return false;
 	}
 
